09558838140781419dc6aeeea7d868b87c28e8a2
Vitória Martins <133159073+queenvickyyyyyyyyyyyyyyyyyyyy@users.noreply.github.com>
2025-11-07 14:10:48 -0300

    Update Turma.java

--- Turma.java
+++ Turma.java
@@ -1,14 +1,13 @@
 public class Turma {
     private Aluno[] alunos;
     private int quantidade;
-    
-    // Construtor
+
     public Turma() {
         this.alunos = new Aluno[5]; // vetor de até 5 alunos
         this.quantidade = 0;
     }
     
-    // Método para adicionar aluno
+//adicionar
     public void adicionarAluno(Aluno aluno) {
         if (quantidade < alunos.length) {
             alunos[quantidade] = aluno;
@@ -19,7 +18,7 @@ public class Turma {
         }
     }
     
-    // Método para listar todos os alunos
+//listar
     public void listarAlunos() {
         System.out.println("\n=== Lista de Alunos ===");
         if (quantidade == 0) {
@@ -32,13 +31,13 @@ public class Turma {
         }
     }
     
-    // Método para buscar aluno por matrícula
-    public Aluno buscarPorMatricula(String matricula) {
+    //buscar
+    public Aluno buscarPorMatricula(Sartring matricula) {
         for (int i = 0; i < quantidade; i++) {
             if (alunos[i].getMatricula().equals(matricula)) {
                 return alunos[i];
             }
         }
-        return null; // não encontrado
+        return null
     }
 }
