d113d09b98f127b566b2749c5818827d2328711e
Vitória Martins <133159073+queenvickyyyyyyyyyyyyyyyyyyyy@users.noreply.github.com>
2025-11-07 14:12:04 -0300

    Refactor Aluno class methods and comments

--- Aluno.java
+++ Aluno.java
@@ -3,27 +3,23 @@ public class Aluno {
     private String matricula;
     private double nota1;
     private double nota2;
-    
-    // Construtor
+
     public Aluno(String nome, String matricula, double nota1, double nota2) {
         this.nome = nome;
         this.matricula = matricula;
         this.nota1 = nota1;
         this.nota2 = nota2;
     }
-    
-    // Método para calcular a média
+
     public double calcularMedia() {
         return (nota1 + nota2) / 2.0;
     }
     
-    // Método para verificar a situação
     public String situacao() {
         double media = calcularMedia();
         return media >= 7.0 ? "Aprovado" : "Reprovado";
     }
     
-    // Método para exibir informações do aluno
     public void exibirInfo() {
         double media = calcularMedia();
         String situacao = situacao();
@@ -32,7 +28,6 @@ public class Aluno {
                          nome, matricula, nota1, nota2, media, situacao);
     }
     
-    // Getters (úteis para a busca)
     public String getMatricula() {
         return matricula;
     }
